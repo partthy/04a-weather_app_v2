d76562bfe57f0e156c504cd9768155f2894de931
partthy <partthy@gmaiil.com>
2025-05-02 22:31:10 -0500

    API Key Removed

--- scriptv2.js
+++ scriptv2.js
@@ -1,4 +1,4 @@
-const APIKEY = "207c7f6fcd5639c8df8a2d845f56cea6";
+const APIKEY = "";
 
 const searchBox = document.querySelector(".search input");
 const searchBtn = document.querySelector(".search button");
